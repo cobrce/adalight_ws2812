14c6b92368803564bac3b50d61de797733f78f20
cobrce <cob_rce@hotmail.com>
2019-12-25 12:39:36 +0100

    Added manually update brightness (from device), added ESP8266 configuration

--- Adalight_WS2812.ino
+++ Adalight_WS2812.ino
@@ -6,19 +6,36 @@
  * @library: FastLED v3.001
  * @date: 11/22/2015
  */
+
+// #define ESP8266
+
+#ifdef ESP8266
+#define FASTLED_ESP8266_D1_PIN_ORDER
+#endif
+
 #include <FastLED.h>
-#define NUM_LEDS 80
+
+
+#ifdef ESP8266
+#define FASTLED_ESP8266_D1_PIN_ORDER
+#define DATA_PIN D6
+#define ANALOG_PIN A0
+#define serialRate 1000000
+#else
 #define DATA_PIN 6
 #define ANALOG_PIN A7
-
 // Baudrate, higher rate allows faster refresh rate and more LEDs (defined in /etc/boblight.conf)
-#define serialRate 1000000
+#define serialRate 250000
+#endif
+#define NUM_LEDS 80
 
 // Adalight sends a "Magic Word" (defined in /etc/boblight.conf) before sending the pixel data
 uint8_t prefix[] = {'A', 'd', 'a'}, hi, lo, chk, i;
 
 // Initialise LED-array
 CRGB leds[NUM_LEDS];
+
+#define UPDATE_BRIGHTNESS_MANUALLY
 
 void setup() {
   // Use NEOPIXEL to keep true colors
@@ -42,14 +59,65 @@ void setup() {
 
 unsigned long prevUpdate = 0;
 #define UPDATE_DELAY 100
-void UpdateBrightness()
+
+void UpdateBrightness(bool force = false)
 {
+    #define N_SAMPLES 10
+    static bool firstSample = true;
+    static unsigned char samples[N_SAMPLES];
+    static char sampleIndex = 0;
+    static unsigned char prevMean = 0;
+
     unsigned long now = millis();
-    if (prevUpdate == 0 || (now - prevUpdate)>=UPDATE_DELAY)
+    if (force || prevUpdate == 0 || (now - prevUpdate)>=UPDATE_DELAY)
     {
         prevUpdate = now;
         int value = analogRead(ANALOG_PIN);
+
+        #ifdef UPDATE_BRIGHTNESS_MANUALLY
+        // convert from 900-100 scale to 20%-100% scale to 50-255 scale
+        value = constrain(value,100,900);
+        float ratio = (value - 100) * ((20.0 - 100.0) / (900.0-100.0)) + 100.0;
+        ratio = constrain(ratio,20.0,100.0);
+        ratio /=100.0;
+        unsigned int scale = 255*ratio;
+        scale = constrain(scale,50,255);
+
+        // fill sample vector if it's the first sample        
+        if (firstSample)
+        {
+            firstSample = false;
+            for (int i = 0;i<N_SAMPLES;i++)
+                samples[i] = scale;
+        }
+
+        // set current scale in sample vector
+        // sampleIndex is a rotating index for samles vector
+        // (a fast/lazy way to implement a queue)
+        samples[sampleIndex] = scale;
+        sampleIndex = (sampleIndex+1)%N_SAMPLES;
+
+        // calculate mean of samples
+        unsigned int sum = 0;
+        for (int i = 0 ;i<N_SAMPLES;i++)
+            sum+=samples[i];
+        
+        char mean = sum/N_SAMPLES;
+
+        Serial.print((unsigned int)mean);
+        Serial.println();
+        // if mean changes by 5 update brigtness
+        if (abs(prevMean-mean)>5)
+        {
+            prevMean = mean;
+            FastLED.setBrightness(mean);
+        }
+
+        // display
+        FastLED.show();
+        #else
         Serial.println(value);
+        #endif
     }
 }
 
@@ -77,7 +145,6 @@ void loop() {
     i=0;
     goto waitLoop;
   }
-  
   memset(leds, 0, NUM_LEDS * sizeof(struct CRGB));
   // Read the transmission data and set LED values
   for (uint8_t i = 0; i < NUM_LEDS; i++) {
@@ -92,7 +159,10 @@ void loop() {
     leds[i].g = g;
     leds[i].b = b;
   }
-  
+  #ifdef UPDATE_BRIGHTNESS_MANUALLY
+  UpdateBrightness(true);
+  #else
   // Shows new values
   FastLED.show();
+  #endif
 }
